053ca06dc3e0c91a87017a1adc23405d7d3fb264
McGoblin <mcgoblin@mail.ru>
2017-05-01 22:56:42 +0500

    Определяем контроллер и акшн

--- www/index.php
+++ www/index.php
@@ -1,5 +1,9 @@
 <?php
-
+    //Определяем какой контролер будет работать
     $controllerName = isset($_GET['controller'])?ucfirst($_GET['controller']):'Index';
-    echo "подключаем $controllerName";
+    echo "подключаем контролер $controllerName <br>";
+
+    //Определяем какой контролер будет работать
+    $actionName = isset($_GET['action'])?ucfirst($_GET['action']):'Index';
+    echo "подключаем экшн $actionName <br>";
 ?>

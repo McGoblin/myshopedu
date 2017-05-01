8543376fe63a7c66bce0b080889a8a340d30529e
McGoblin <mcgoblin@mail.ru>
2017-05-01 23:51:41 +0500

    Документирование кода

--- library/mainFunction.php
+++ library/mainFunction.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @param $controllerName имя контроллера
+ * @param string $actionName имя экшена
+ */
 function loadPage ($controllerName, $actionName = 'index'){
     include_once PathPrefix.$controllerName.PathPostfix;
     //подключаем функцию

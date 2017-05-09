d5008860273397325242c7a3134b345ea4627f72
McGoblin <mcgoblin@mail.ru>
2017-05-10 00:08:13 +0500

    Передаем продукты в шаблон сайта

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 //Подключение моделей
 include_once '../models/CategoriesModel.php'; //Модель для работы с категориями
+include_once '../models/ProductsModel.php'; //Модель для работы с продуктами
 
 function testAction () {
     echo "IndexController.php > test";
@@ -9,9 +10,11 @@ function testAction () {
 
 function indexAction ($smarty) {
     $rsCategories = getAllMainCatsWithChildren();
+    $rsProducts = getLasProducts(16);
 
     $smarty->assign('pageTitle', 'Главная страница');// Передаем название страницы
     $smarty->assign('rsCategories', $rsCategories); // Передаем категории сайта
+    $smarty->assign('rsProducts', $rsProducts); // Передаем продукты сайта
 
     loadTemlate($smarty, 'header');
     loadTemlate($smarty, 'index');

10e739c7c1a26a9bf577ce52f792163ba3086cf4
McGoblin <mcgoblin@mail.ru>
2017-05-19 15:45:21 +0500

    Получения списка категорий

---
+++ models/CategoriesModel.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Модель формирования списка категорий и работы с таблицей категорий (categories)
+ */
+
+/**
+ * Получение категорий сайта с привязками дочерних категорий
+ * @return array категорий сайта
+ */
+function getAllMainCatsWithChildren () {
+    $sql = 'SELECT *
+            FROM `categories`
+            WHERE `parent_id` = 0';
+
+    $res = mysqli_query(dbConect(), $sql);
+
+    $smartyres = array();
+    while ($row = mysqli_fetch_assoc($res)){
+        $rsChildren = getChildrenForCat($row['id']);
+        if ($rsChildren){
+            $row['children'] = $rsChildren;
+        }
+        $smartyres[] = $row;
+    };
+    return $smartyres;
+}
+
+/**
+ * Функция получает дочерние категории
+ * @param $id категории родителя
+ * @return array дочерних категорий
+ */
+function getChildrenForCat($id){
+    $sql = "SELECT * FROM `categories` WHERE `parent_id` = '{$id}'";
+    $res = mysqli_query(dbConect(), $sql);
+    return createSmartyRsArray($res);
+}
+
+function getCatById ($catID){
+    $catID = intval($catID);
+    $sql = "
+    SELECT * 
+    FROM `categories`
+    WHERE `id` = '$catID'";
+    $res = mysqli_query(dbConect(), $sql);
+    return createSmartyRsArray($res);
+}

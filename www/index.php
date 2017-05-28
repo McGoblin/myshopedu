25d3813c70bdf335eeca11412b6a334238b74bf3
McGoblin <mcgoblin@mail.ru>
2017-05-29 01:47:21 +0500

    Сессия пользователя

--- www/index.php
+++ www/index.php
@@ -1,21 +1,27 @@
 <?php
-    session_start();
+session_start();
 
-    if(!isset($_SESSION['cart'])){
-        $_SESSION['cart'] = array();
-    }
+if(!isset($_SESSION['cart'])){
+    $_SESSION['cart'] = array();
+}
+
+if(!isset($_SESSION['user'])){
+    $_SESSION['user'] = array();
+}
+
+//    echo dump($_SESSION);
 
 //Подключаем файлы
-    include_once '../config/config.php'; //Настройки
-    include_once "../config/db.php"; //подключение к базе данных
-    include_once '../library/mainFunction.php'; //файл основных функций
+include_once '../config/config.php'; //Настройки
+include_once "../config/db.php"; //подключение к базе данных
+include_once '../library/mainFunction.php'; //файл основных функций
 
-    //Определяем какой контролер будет работать
-    $controllerName = isset($_GET['controller'])?ucfirst($_GET['controller']):'Index';
+//Определяем какой контролер будет работать
+$controllerName = isset($_GET['controller'])?ucfirst($_GET['controller']):'Index';
 
-    //Определяем какой контролер будет работать
-    $actionName = isset($_GET['action'])?ucfirst($_GET['action']):'Index';
-    $smarty->assign('cartCntItems', count($_SESSION['cart']));
-    loadPage($smarty, $controllerName, $actionName);
+//Определяем какой контролер будет работать
+$actionName = isset($_GET['action'])?ucfirst($_GET['action']):'Index';
+$smarty->assign('cartCntItems', count($_SESSION['cart']));
+loadPage($smarty, $controllerName, $actionName);
 
-    dbDisconnect();
+dbDisconnect();

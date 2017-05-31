4844fcaa0d95bb447deb9bcada19f3d39caf9ba3
McGoblin <mcgoblin@mail.ru>
2017-05-31 23:45:38 +0500

    Добавлена функция для аякс запросса, которая обновляет данные в базе

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -100,3 +100,43 @@ function indexAction ($smarty){
     loadTemlate($smarty, 'user');
     loadTemlate($smarty, 'footer');
 }
+
+function updateAction () {
+    if (!isset($_SESSION['user'])){
+        redirect('/');
+    }
+
+    $resData=array();
+    $name=isset($_REQUEST['name'])?$_REQUEST['name']:null;
+    $phone=isset($_REQUEST['phone'])?$_REQUEST['phone']:null;
+    $adress=isset($_REQUEST['adress'])?$_REQUEST['adress']:null;
+    $pwd1=isset($_REQUEST['pwd1'])?$_REQUEST['pwd1']:null;
+    $pwd2=isset($_REQUEST['pwd2'])?$_REQUEST['pwd2']:null;
+    $curPwd=isset($_REQUEST['curPwd'])?$_REQUEST['curPwd']:null;
+
+    $curPwdMD5 = md5($curPwd);
+    if(!$curPwd||($_SESSION['user']['pwd']) != $curPwdMD5){
+        $resData['success']=0;
+        $resData{'message'}="Не верный пароль";
+        echo json_encode($resData);
+        return false;
+    }
+
+    $res = updateUserData($name, $phone, $adress, $pwd1, $pwd2, $curPwdMD5);
+    if ($res){
+        $resData['success']=1;
+        $resData{'message'}="Данные сохранены";
+        $resData{'userName'}=$name;
+
+        $_SESSION['user']['name'] = $name;
+        $_SESSION['user']['phone'] = $phone;
+        $_SESSION['user']['adress'] = $adress;
+        $_SESSION['user']['pwd'] = $curPwdMD5;
+        $_SESSION['user']['displayName'] = $name?$name:$_SESSION['user']['email'];
+
+    }else{
+        $resData['success']=0;
+        $resData{'message'}="Ошибка обновления данных";
+    }
+    echo json_encode($resData);
+}

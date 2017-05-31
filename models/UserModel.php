f672bc04f44d246c5c6fc69eea28448897d4ca42
McGoblin <mcgoblin@mail.ru>
2017-05-31 18:06:33 +0500

    Обновление данных пользователя

--- models/UserModel.php
+++ models/UserModel.php
@@ -86,3 +86,36 @@ function loginUser($email, $pwd){
 
     return $res;
 }
+
+function updateUserData ($name, $phone, $adress, $pwd1, $pwd2, $curPwd){
+    $name = stringToSQL($name);
+    $email = stringToSQL($_SESSION['user']['email']);
+    $phone = stringToSQL($phone);
+    $adress = stringToSQL($adress);
+
+    $pwd1= trim($pwd1);
+    $pwd2= trim($pwd2);
+
+    $newPwd=null;
+    if ($pwd1 && ($pwd1==$pwd2)){
+        $newPwd = md5($pwd1);
+    }
+
+    $sql = "UPDATE `user` SET ";
+    if ($newPwd){
+        $sql = "`pwd` = '{$newPwd}', ";
+    }
+
+    $sql = "
+    `name` = '{$newPwd}',
+    `phone` = '{$phone}',
+    `adress` = '{$adress}'
+    WHERE 
+    `email` = '{$email}' AND `pwd` = '{$curPwd}'
+    LIMIT 1
+    ";
+
+    $res = mysqli_query(dbConnect(), $sql);
+
+    return $res;
+}
